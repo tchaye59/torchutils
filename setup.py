707f563d102e28d45719ad93eeed12b3b1929668
Jude TCHAYE <tchaye59@gmail.com>
2021-06-19 13:33:33 +0800

    update project structure

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("LICENSE", "r", encoding="utf-8") as f:
 
 setup(
     name='torchutils',
-    version='0.0.2',
+    version='0.0.3',
     packages=setuptools.find_packages(),
     url='https://github.com/tchaye59/torchutils',
     license=LICENSE,
